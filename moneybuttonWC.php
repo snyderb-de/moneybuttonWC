ec472d73e55c1dc19e827a00ba7641213721df44
Bryan Snyder <snyderb/de@gmail.com>
2019-08-05 13:33:26 -0400

    updates to payment skeleton
    created TODOs in moneybuttonWC,php

--- moneybuttonWC.php
+++ moneybuttonWC.php
@@ -15,9 +15,9 @@
  *
  */
 
-add_filter('woocommerce_payment_gateways', 'moneybuttonWC_as_class');
-function moneybuttonWC_as_class($gateways) {
-    $gateways[] = 'moneybuttonWC_gateway';
+add_filter('woocommerce_payment_gateways', 'mbwc_class');
+function mbwc_class($gateways) {
+    $gateways[] = 'MBWC_Gateway';
     return $gateways;
 }
 
@@ -27,3 +27,16 @@ function moneybuttonWC_as_class($gateways) {
  *
  */
 
+ add_action('plugins_loaded', 'mbwc_init');
+ function mbwc_init() {
+    class MBWC_Gateway extends WC_Payment_Gateway {
+        /**
+         * TODO: create constructor
+         * TODO: create options
+         * TODO: create payment fields
+         * TODO: payment scripts
+         * TODO: fields validation
+         * TODO: add webhooks -> check MB docs
+         */
+    }
+ }
